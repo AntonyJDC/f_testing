b3ae3c9decbef11bc6806712b1bc407303d161d1
Antony Dominguez <jaconsuegra@uninorte.edu.co>
2025-03-13 22:31:54 -0500

    Add test: widget login validación @ email

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,11 +5,28 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:f_testing_template/ui/pages/authentication/login.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:get/get.dart';
 
 void main() {
-  testWidgets(
-      'Widget login validación @ email', (WidgetTester tester) async {});
+  testWidgets('Widget login validación @ email', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const GetMaterialApp(
+        home: LoginScreen(email: 'test@example.com', password: '123456'),
+      ),
+    );
+
+    final emailField = find.byKey(const Key('TextFormFieldLoginEmail'));
+    final submitButton = find.byKey(const Key('ButtonLoginSubmit'));
+
+    await tester.enterText(emailField, 'usuario_invalido');
+    await tester.tap(submitButton);
+    await tester.pump();
+
+    expect(find.text('Enter valid email address'), findsOneWidget);
+  });
 
   testWidgets('Widget login validación campo vacio email',
       (WidgetTester tester) async {});
